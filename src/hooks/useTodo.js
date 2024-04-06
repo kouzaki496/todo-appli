55e5fabde57e91d8cfbe4a539b170553b6f792e8
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2024-04-06 21:31:10 +0900

    コメントの修正

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -6,43 +6,35 @@ import * as todoData from "../apis/todos";
 export const useTodo = () => {
   const [todoList, setTodoList] = useState([]);
   const [todoText, setTodoText] = useState([]);
-  const todoDataUrl = "http://localhost:3100/todos"; //モックサーバーのURL
-
-  // useEffect(() => {
-  //   todoData.getAllTodosData().then((todo) => {
-  //     setTodoList([...todo].reverse());
-  //   })
-  // }, []);
+  // const todoDataUrl = "http://localhost:3100/todos"; //モックサーバーのURL
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(todoDataUrl);
-      setTodoList(response.data);
-    };
-    fetchData();
+    todoData.getAllTodosData().then((todo) => {
+      setTodoList([...todo].reverse());
+    })
   }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await axios.get(todoDataUrl);
+  //     setTodoList(response.data);
+  //   };
+  //   fetchData();
+  // }, []);
 
 
   //ステータスの変更
   const toggleTodoListItemStatus = (id, done) => {
     const todoItem = todoList.find((item) => item.id === id);
     const newTodoItem = { ...todoItem, done: !done } //doneを反転
-    const newTodoList = todoList.map((item) =>{
-      return item.id !== id ? item : newTodoItem;
-    });
-      setTodoList(newTodoList);
-  };
-  // const toggleTodoListItemStatus = (id, done) => {
-  //   const todoItem = todoList.find((item) => item.id === id);
-  //   const newTodoItem = { ...todoItem, done: !done } //doneを反転
 
-  //   todoData.updateTodoData(id, newTodoItem).then((updatedTodo) => {
-  //     const newTodoList = todoList.map((item) =>
-  //       item.id !== updatedTodo.id ? item : updatedTodo
-  //     );
-  //     setTodoList(newTodoList);
-  //   });
-  // };
+    todoData.updateTodoData(id, newTodoItem).then((updatedTodo) => {
+      const newTodoList = todoList.map((item) =>
+        item.id !== updatedTodo.id ? item : updatedTodo
+      );
+      setTodoList(newTodoList);
+    });
+  };
 
   //TODOの追加
   const addTodoListItem = (todoText) => {
@@ -51,35 +43,20 @@ export const useTodo = () => {
       "content": todoText,
       "done": false
     };
-      setTodoList([...todoList, newTodoItem]);
-      setTodoText("");
+    return todoData.addTodoData(newTodoItem).then((addTodo) => {
+      setTodoList([addTodo, ...todoList]);
+    });
   };
-  // const addTodoListItem = (todoText) => {
-  //   const newTodoItem = {
-  //     "id": ulid(),
-  //     "content": todoText,
-  //     "done": false
-  //   };
-  //   return todoData.addTodoData(newTodoItem).then((addTodo) => {
-  //     setTodoList([addTodo, ...todoList]);
-  //   });
-  // };
 
   //TODOの削除
   const deleteTodoListItem = (id) => {
+    todoData.deleteTodoData(id).then((deleteTodoListItemId) => {
       const newTodoList = todoList.filter(
-        (item) => item.id !== id
+        (item) => item.id !== deleteTodoListItemId
       );
       setTodoList(newTodoList);
+    })
   }
-  // const deleteTodoListItem = (id) => {
-  //   todoData.deleteTodoData(id).then((deleteTodoListItemId) => {
-  //     const newTodoList = todoList.filter(
-  //       (item) => item.id !== deleteTodoListItemId
-  //     );
-  //     setTodoList(newTodoList);
-  //   })
-  // }
   // const deleteTodoListItem = (id) => {
   //   const newTodo = [...todoList];
   //   const isDeleteTodo = (todoList) => todoList.id === id;
